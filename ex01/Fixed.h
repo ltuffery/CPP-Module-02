6185dd1c57347de615a7ba735c21524e09f9fbfa
ltuffery <123221865+ltuffery@users.noreply.github.com>
2023-07-25 09:59:13 +0200

    update ex01

---
+++ ex01/Fixed.h
@@ -0,0 +1,28 @@
+#ifndef FIXED_H
+# define FIXED_H
+
+#include <ostream>
+
+class Fixed {
+
+	private:
+		int comma;
+		const static int bits = 8; 
+
+	public:
+		Fixed( void );
+		Fixed( const int n );
+		Fixed( const float n );
+		Fixed( const Fixed &fixed );
+		~Fixed( void );
+		Fixed &operator=( const Fixed &fixed );
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
+		float toFloat( void ) const;
+		int toInt( void ) const;
+
+};
+
+std::ostream & operator<<(std::ostream & os, Fixed fixed);
+
+#endif
