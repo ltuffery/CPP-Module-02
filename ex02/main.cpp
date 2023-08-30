d40078ddb2556d07259c82830922dc4e3eae0415
ltuffery <123221865+ltuffery@users.noreply.github.com>
2023-08-30 16:56:23 +0200

    add tests for ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -15,9 +15,42 @@ int main( void ) {
 		std::cout << Fixed::max( a, b ) << std::endl;
 		std::cout << Fixed::min( a, b ) << std::endl;
 	}
+	std::cout << "\n-----------------\n" << std::endl;
 	{
 		Fixed a(2);
 		Fixed b(3);
+
+		if (a < b)
+			std::cout << "a < b" << std::endl;
+		if (a > b)
+			std::cout << "a > b" << std::endl;
+		if (a <= b)
+			std::cout << "a <= b" << std::endl;
+		if (a >= b)
+			std::cout << "a >= b" << std::endl;
+		if (a == b)
+			std::cout << "a == b" << std::endl;
+		if (a != b)
+			std::cout << "a != b" << std::endl;
+	}
+	std::cout << "\n-----------------\n" << std::endl;
+	{
+		Fixed a(2);
+		Fixed const b(5.5f);
+
+		std::cout << a + b << std::endl;
+		std::cout << a - b << std::endl;
+		std::cout << a * b << std::endl;
+		std::cout << a / b << std::endl;
+	}
+	std::cout << "\n-----------------\n" << std::endl;
+	{
+		Fixed a(3);
+
+		std::cout << a++ << std::endl;
+		std::cout << ++a << std::endl;
+		std::cout << a-- << std::endl;
+		std::cout << --a << std::endl;
 	}
 	return 0;
 }
